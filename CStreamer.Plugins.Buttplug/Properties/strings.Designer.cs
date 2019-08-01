58afc2c10ea2ad107864adfce782c1e92b9a7c28
Lucky Skebe <fmann12345@gmail.com>
2019-08-01 07:26:24 +0200

    missing commit

--- CStreamer.Plugins.Buttplug/Properties/strings.Designer.cs
+++ CStreamer.Plugins.Buttplug/Properties/strings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace SharPipes.Pipes.Buttplug.Properties {
+namespace CStreamer.Plugins.Buttplug.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace SharPipes.Pipes.Buttplug.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("SharPipes.Pipes.Buttplug.Properties.strings", typeof(strings).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("CStreamer.Plugins.Buttplug.Properties.strings", typeof(strings).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
